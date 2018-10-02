c560c134a32c6985d5dee7ea32c9145fb3c2742a
Jacob Tomlinson <jacob.tomlinson@informaticslab.co.uk>
2018-10-02 08:05:18 +0100

    Fix deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,16 +8,16 @@ export REGION='eu-west-1'
 #####
 # amos-latest-forecast-rename
 
-cd amos-latest-forecast-rename
+cd amos-latest-forecast-rename/src
 zip -r code.zip lambda_function.py
 aws lambda --region ${REGION} update-function-code --function-name amos-latest-forecast-rename --zip-file fileb://$PWD/code.zip
-cd ..
+cd ../..
 
 
 #####
 # amos-latest-forecast-copy
 
-cd amos-latest-forecast-copy
+cd amos-latest-forecast-copy/src
 zip -r code.zip lambda_function.py
 aws lambda --region ${REGION} update-function-code --function-name amos-latest-forecast-copy --zip-file fileb://$PWD/code.zip
-cd ..
+cd ../..
