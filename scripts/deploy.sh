6a37271a66351b1123301c26f86db3d47688e679
Debbie O'Sullivan <debbieosullivan@hineshs-mbp.home>
2018-10-12 23:41:04 +0100

    bug fixes

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,16 +8,25 @@ export REGION='eu-west-1'
 #####
 # amos-latest-forecast-rename
 
-cd amos-latest-forecast-rename/src
-zip -r code.zip lambda_function.py
-aws lambda --region ${REGION} update-function-code --function-name amos-latest-forecast-rename --zip-file fileb://$PWD/code.zip
-cd ../..
+#cd amos-latest-forecast-rename
+#zip -r code.zip lambda_function.py
+#aws lambda --region ${REGION} update-function-code --function-name amos-latest-forecast-rename --zip-file fileb://$PWD/code.zip
+#cd ..
 
 
 #####
 # amos-latest-forecast-copy
 
-cd amos-latest-forecast-copy/src
-zip -r code.zip lambda_function.py
-aws lambda --region ${REGION} update-function-code --function-name amos-latest-forecast-copy --zip-file fileb://$PWD/code.zip
+#cd amos-latest-forecast-copy
+#zip -r code.zip lambda_function.py
+#aws lambda --region ${REGION} update-function-code --function-name amos-latest-forecast-copy --zip-file fileb://$PWD/code.zip
+#cd ..
+
+####
+# amos-rename-and-revolume
+
+cd amos-rename-and-revolume
+cd src 
+zip -r ../deploy.zip lambda_function.py
+aws lambda --region ${REGION} update-function-code --function-name amos-rename-and-revolume --zip-file fileb://$PWD/../deploy.zip
 cd ../..
